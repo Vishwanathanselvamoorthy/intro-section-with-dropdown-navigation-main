892dd46c273d16990d562a102e884e1e8beb6e06
Vishwa <viswanathanselvamoorthy@gmail.com>
2024-04-23 11:38:07 +0530

    .

--- script.js
+++ script.js
@@ -10,12 +10,27 @@ const featuresMenu = document.querySelector(".features-menu");
 
 const featuresOptions = document.querySelector(".features-options");
 
+const companyMenu = document.querySelector(".company-menu");
+
+const companyOptions = document.querySelector(".company-options");
+
+const blackSpace = document.querySelector(".black");
+
 menuClose.addEventListener("click", function () {
   sideBar.classList.add("hidden");
-  homePage.classList.remove("black");
+  //   homePage.classList.remove("black");
+  blackSpace.classList.add("hidden");
 });
 
 menuOpen.addEventListener("click", function () {
   sideBar.classList.remove("hidden");
-  homePage.classList.add("black");
+  //   homePage.classList.add("black");
+  blackSpace.classList.remove("hidden");
+});
+
+featuresMenu.addEventListener("click", function () {
+  featuresOptions.classList.toggle("hidden");
+});
+companyMenu.addEventListener("click", function () {
+  companyOptions.classList.toggle("hidden");
 });
